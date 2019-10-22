25af293edeeae4403c2d0fb9dbd3f1781d09af45
samoncrief <51212088+samoncrief@users.noreply.github.com>
2019-10-22 09:53:50 -0400

    update sketch

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,29 +1,40 @@
 var canvasFullw = 1200;
 var canvasFullh = 800;
 //planet size and color variables
+var planetSizes = [Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur];
+var planetColors = [Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur];
 //previous time
+var prevTime;
 //delta time
+var deltaTime;
 //time-theta scale
-//theta
+var timeTheta;
+var theta;
 //display window boolean
+var infoDisplay;
 //planet info array
-//current planet info
+var planetInfo = [Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur];
+var currentPlanet;
 
 function setup() {
   // put setup code here
   createCanvas(canvasFullw, canvasFullh);
+  prevTime = millis();
   background(0, 255, 0);
 }
 
 function draw() {
   // put drawing code here
   // default fill,stroke,strokeWeight
-  fill(0);
+  fill(0,0,0);
   stroke("black");
   strokeWeight(1);
 
   //update delta
+  deltaTime = millis() - prevTime;
   //update previous time
+  prevTime = millis();
+  debug.log("Delta: " + deltaTime);
   if (/*display boolean*/ true){
     //display info for current selection
   }
