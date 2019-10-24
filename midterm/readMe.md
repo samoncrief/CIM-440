3efce32b9f7e779fe4ea8bfbe7f41bc5edaeb703
samoncrief <51212088+samoncrief@users.noreply.github.com>
2019-10-24 11:29:19 -0400

    sketch, readMe

---
+++ midterm/readMe.md
@@ -0,0 +1,1 @@
+An interactive solar system with simulated orbits and planet info.
--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,8 +1,22 @@
 var canvasFullw = 1200;
 var canvasFullh = 800;
 //planet size and color variables
-var planetSizes = ["Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur"];
-var planetColors = ["Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur"];
+var planetSizes = [0.38,0.95,1,.53,11.2,9.45,4,3.88];
+var planetSizeScale = 10;
+var planetDistances = [35.98,67.24,92.96,141.6,483.8,890.8,1784,2793];
+var planetDistanceScale = 600/2793;
+
+/// Planet sizes and distances:
+/// Mercury: 2440km (.38), 35.98 mil.mi
+/// Venus: 6052km (.95), 67.24 mil.mi
+/// Earth: 6371km (1), 92.96 mil.mi
+/// Mars: 3390km (.53), 141.6 mil.mi
+/// Jupiter: 69911km (11.2), 483.8 mil.mi
+/// Saturn: 58232km (9.45), 890.8 mil.mi
+/// Uranus: 25632km (4), 1.784 bil.mi
+/// Neptune: 24622km (3.88), 2.793 bil.mi
+
+var planetColors = [("gray"),("tan"),("blue"),("red"),("orange"),("tan"),("teal"),("blue")];
 //previous time
 var prevTime;
 //delta time
@@ -13,7 +27,7 @@ var theta;
 //display window boolean
 var infoDisplay;
 //planet info array
-var planetInfo = ["Me,Ve,Ea,Ma,Ju,Sa,Ne,Ur"];
+var planetInfo = ["Me,Ve,Ea,Ma,Ju,Sa,Ur,Ne"];
 var currentPlanet;
 
 function setup() {
@@ -47,10 +61,13 @@ function draw() {
     //update theta
 
     //dummy object
-    fill("blue");
-    circle(canvasFullw/2 + 100*(cos(millis()*timeTheta)), canvasFullh/2 + 100*(sin(millis()*timeTheta)), 30);
+    //fill("blue");
+    //circle(canvasFullw/2 + 100*(cos(millis()*timeTheta)), canvasFullh/2 + 100*(sin(millis()*timeTheta)), 30);
+
     for(var i = 0; i < 8; i++){
       //create planet[i] at x*cos(theta),y*sin(theta)
+      fill(planetColors[i]);
+      circle(canvasFullw/2 + planetDistances[i]*planetDistanceScale*(cos(millis()*timeTheta)), canvasFullh/2 + planetDistances[i]*(sin(millis()*timeTheta)),planetSizes[i]*planetSizeScale);
     }
     fill("gray");
     for(var i = 0; i < 8; i++){
