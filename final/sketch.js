c9c4a03fb9fde4ba16089840fec97b20ba2e9b3a
samoncrief <51212088+samoncrief@users.noreply.github.com>
2019-12-08 18:12:38 -0500

    Update sketch.js

--- final/sketch.js
+++ final/sketch.js
@@ -2,14 +2,9 @@ var canvasFull = 800;
 
 var blockArray = {};
 
-function blockForm(entry){
-  this.entry = entry % 5;
-  this.exit = (Math.random() * blockArray[this.entry].length) % blockArray[this.entry].length;
-  this.sprite = blockArray[this.entry][this.exit];
-}
-
 var menu = false;
 var player = null;
+var row = null;
 
 function setup() {
   // put setup code here
@@ -18,8 +13,10 @@ function setup() {
   player = createSprite(canvasFull/2,canvasFull/2,127,127);
   player.addAnimation('normal', 'boxGreen.png');
 
-  wall = createSprite(canvasFull/2,-200,127,127);
-  wall.addAnimation('normal','boxYellow.png');
+  blocks = new Group();
+
+  //instantiate wall configurations
+  blockArray = new Array(new Array(),new Array(),new Array(),new Array(),new Array());
 }
 
 function draw() {
@@ -41,17 +38,19 @@ function draw() {
     }
     else{
       //main game loop
+      if(row == null){
+        createRow(0);
+      }
+      else if(row.position.y > 0){
+        createRow(0);
+      }
 
       //move sprites
       player.velocity.x = (mouseX-player.position.x)/10;
       player.velocity.y = (canvasFull/2 - player.position.y)/10;
-      wall.velocity.y = 5;
-      if(wall.position.y > 950){
-        wall.position.y = -200;
-      }
 
       //collisions
-      player.collide(wall);
+      player.collide(blocks);
 
       //if player goes out of bounds, they lose
 
@@ -65,4 +64,45 @@ function draw() {
 
 function mouseClicked(){
   //pause/unpause game on mouse click
+}
+
+function createBlock(X, Y){
+  block = createSprite(X,Y,127,127);
+  block.addAnimation('normal','boxYellow.png');
+  block.velocity = 5;
+  block.life = 50;
+  blocks.add(block);
+  row = block;
+}
+
+function createRow(index){
+  switch(index){
+    case 1:
+      //wall in middle
+    case 2:
+      //leftmost exit
+    case 3:
+      //left exit
+    case 4:
+      //middle exit
+    case 5:
+      //right exit
+    case 6:
+      //rightmost exit
+    case 7:
+      //left/mid, take left
+    case 8:
+      //left/mid, take mid
+    case 9:
+      //left/right, take left
+    case 10:
+      //left/right, take right
+    case 11:
+      //mid/right, take mid
+    case 12:
+      //mid/right, take
+    default:
+      createBlock(0,-127);
+      createBlock(canvasFull,-127);
+  }
 }
