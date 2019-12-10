c8f058efdc10d8b70254bec28c2fd8c0a4152d28
samoncrief <51212088+samoncrief@users.noreply.github.com>
2019-12-10 09:46:12 -0500

    Update sketch.js

--- final/sketch.js
+++ final/sketch.js
@@ -11,7 +11,7 @@ var ints6 = [0,1,2,3,4,5];
 var optionsFull = [2,3,4,5,6,7,8];
 var optionsL = [2,3,7];
 var optionsR = [5,6,8];
-var menuText = "Welcome to Boxed!\n"
+var menuText = "Welcome to Boxed!\nClick to continue";
 
 var lastRow = 0;
 var increment = 1;
@@ -38,7 +38,7 @@ function draw() {
   if(menu){
     //initial menu
     textAlign(CENTER);
-    textSize(20);
+    textSize(30);
     rect(canvasFull/4,canvasFull/4,canvasFull/2,canvasFull/2);
     fill("red");
     text(menuText,canvasFull/2,canvasFull/2);
@@ -100,6 +100,7 @@ function draw() {
 
       //if player goes out of bounds, they lose
       if(player.position.y > canvasFull - 10 - 24){
+        menuText = "Game over\nClick to retry";
         menu = true;
       }
 
